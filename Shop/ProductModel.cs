a155a00241615db55a9a9acd6cc67eaee12da02d
Rashid Isaev <isarutul@yandex.ru>
2025-04-30 17:54:33 +0300

    Add ProductModel class with INotifyPropertyChanged implementation

---
+++ Shop/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class ProductModel: INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
