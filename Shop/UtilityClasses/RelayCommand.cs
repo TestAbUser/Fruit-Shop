9d9e091fecdb5c36e20b481348d311a2031f4a03
Rashid Isaev <isarutul@yandex.ru>
2025-05-01 17:48:59 +0300

    Add RelayCommand class

---
+++ Shop/UtilityClasses/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace Shop.UtilityClasses
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action? _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action execute) : this(execute, null) { }
+
+        public RelayCommand(Action execute, Func<bool>? canExecute)
+        {
+            _execute = execute
+            ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public virtual bool CanExecute(object? parameter) => _canExecute == null || _canExecute();
+
+        public virtual void Execute(object? parameter) { _execute?.Invoke(); }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
