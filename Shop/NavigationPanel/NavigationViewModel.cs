a0981e556c445ac5514bf65db3bf350bb8884ded
Rashid Isaev <isarutul@yandex.ru>
2025-05-01 18:49:08 +0300

    Add PageViewModel property to NavigatioViewModel

--- Shop/NavigationPanel/NavigationViewModel.cs
+++ Shop/NavigationPanel/NavigationViewModel.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using Shop.UtilityClasses;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Shop.NavigationPanel
 {
     public class NavigationViewModel: INotifyPropertyChanged
     {
+        private IPageViewModel? _pageViewModel;
 
+        public IPageViewModel? PageViewModel
+        {
+            get => _pageViewModel;
+            set
+            {
+                if (_pageViewModel == value) return;
+                _pageViewModel = value;
+                OnPropertyChanged(nameof(PageViewModel));
+            }
+        }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
