981ea31504675de3068a63f0440f0f1c4ad28481
Rashid Isaev <isarutul@yandex.ru>
2025-05-01 15:04:56 +0300

    Move Header-related types to a folder

--- Shop/Header/Header.xaml.cs
+++ Shop/Header/Header.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Shop
+namespace Shop.Header
 {
     /// <summary>
     /// Interaction logic for Header.xaml
--- Shop/Header/HeaderViewModel.cs
+++ Shop/Header/HeaderViewModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shop
+namespace Shop.Header
 {
-    public class HeaderViewModel
+    class HeaderViewModel
     {
     }
 }
