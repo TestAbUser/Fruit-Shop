4fecb3206ceb78e34437ddf7c4d3db3d8d04b3da
Rashid Isaev <isarutul@yandex.ru>
2025-05-02 16:14:56 +0300

    Add Products property to HomePageViewModel

--- Shop/HomePage/HomePageViewModel.cs
+++ Shop/HomePage/HomePageViewModel.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Shop.UtilityClasses;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shop.HomePage
 {
-    public class HomePageViewModel: IPageViewModel
+    public class HomePageViewModel:ObservableObject, IPageViewModel
     {
+        private ProductModel _product;
         public string Name => "Home";
+
+        public HomePageViewModel() 
+        {
+            _product = new ();
+            Products = _product.GetProducts();
+        }
+        public ObservableCollection<ProductModel> Products { get;} = [];
     }
 }
