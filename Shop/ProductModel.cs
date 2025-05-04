52de28e9aac16ab8410cda17329278c20c2a7b64
Rashid Isaev <isarutul@yandex.ru>
2025-05-04 19:03:23 +0300

    Update ProductModel properties to be used for Cart page

--- Shop/ProductModel.cs
+++ Shop/ProductModel.cs
@@ -14,7 +14,8 @@ namespace Shop
     {
         private string? _pictureUrl;
         private string? _name;
-        private int _quantity;
+        private int _quantityInStock;
+        private int _quantityToBuy;
 
         public string? PictureUrl
         {
@@ -37,26 +38,40 @@ namespace Shop
             }
         }
 
-        public int Quantity
+        public int QuantityInStock
         {
-            get => _quantity;
+            get => _quantityInStock;
 
             set
             {
-                if (value == _quantity) return;
-                _quantity = value;
-                OnPropertyChanged(nameof(Quantity));
+                if (value == _quantityInStock) return;
+                _quantityInStock = value;
+                OnPropertyChanged(nameof(QuantityInStock));
             }
         }
+
+        public int QuantityToBuy
+        {
+            get => _quantityToBuy;
+
+            set
+            {
+                if (value == _quantityToBuy) return;
+                _quantityToBuy = value;
+                OnPropertyChanged(nameof(QuantityToBuy));
+                OnPropertyChanged(nameof(IsInStock));
+            }
+        }
+        public bool IsInStock => _quantityToBuy <= _quantityInStock;
 
         public ObservableCollection<ProductModel> Products { get; } = [];
 
         public ObservableCollection<ProductModel> CreateProducts()
         {
-            Products.Add(new ProductModel { Name = "Bananas", PictureUrl = "/Images/bananas.jpg", Quantity = 50 });
-            Products.Add(new ProductModel { Name = "Tomatoes", PictureUrl = "/Images/tomatoes.jpg", Quantity = 150 });
-            Products.Add(new ProductModel { Name = "Apples", PictureUrl = "/Images/apples.jpg", Quantity = 71 });
-            Products.Add(new ProductModel { Name = "Pears", PictureUrl = "/Images/pears.jpg", Quantity = 33 });
+            Products.Add(new ProductModel { Name = "Bananas", PictureUrl = "/Images/bananas.jpg", QuantityInStock = 50 });
+            Products.Add(new ProductModel { Name = "Tomatoes", PictureUrl = "/Images/tomatoes.jpg", QuantityInStock = 150 });
+            Products.Add(new ProductModel { Name = "Apples", PictureUrl = "/Images/apples.jpg", QuantityInStock = 71 });
+            Products.Add(new ProductModel { Name = "Pears", PictureUrl = "/Images/pears.jpg", QuantityInStock = 33 });
             return Products;
         }
     }
