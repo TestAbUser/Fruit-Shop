a6b3981ef0d9d212001468fb1b727727904960af
Rashid Isaev <isarutul@yandex.ru>
2025-05-06 19:06:40 +0300

    Implement validation for PhoneNumber property

--- Shop/UserModelPartial.cs
+++ Shop/UserModelPartial.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Shop
 {
-    public partial class UserModel: IDataErrorInfo
+    public partial class UserModel : IDataErrorInfo
     {
         private string _error = string.Empty;
 
@@ -18,14 +19,39 @@ namespace Shop
         {
             get
             {
-                if (columnName == nameof(Email))
+                switch (columnName)
                 {
-                    if (!EmailValidationRegex.IsValidEmail(Email))
-                    {
-                        return "Email is not valid!";
-                    }
+                    case nameof(Email):
+                        if (!EmailValidationRegex.IsValidEmail(Email))
+                        {
+                            return "Email is not valid!";
+                        }
+                        return string.Empty;
+                    case nameof(PhoneNumber):
+                        if (!ValidatePhoneNumber(PhoneNumber))
+                        {
+                            return "Phone number is not valid!";
+                        }
+                        return string.Empty;
                 }
+
                 return string.Empty;
+            }
+        }
+
+        private bool ValidatePhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                return false;
+            try
+            {
+                return Regex.IsMatch(phoneNumber,
+                    @"^\s*\+?\s*([0-9][\s-]*){9,}$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
             }
         }
     }
