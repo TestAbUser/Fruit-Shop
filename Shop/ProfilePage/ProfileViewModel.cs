0628fd275c8d630a0f40d7cfe99621d03aad1bfe
Rashid Isaev <isarutul@yandex.ru>
2025-05-06 03:16:07 +0300

    Implement Email validation

--- Shop/ProfilePage/ProfileViewModel.cs
+++ Shop/ProfilePage/ProfileViewModel.cs
@@ -1,13 +1,47 @@
-﻿using System;
+﻿using Shop.UtilityClasses;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shop.ProfilePage
 {
-    public class ProfileViewModel: IPageViewModel
+    public class ProfileViewModel: ObservableObject, IPageViewModel, IDataErrorInfo
     {
+        private string ?_email;
+        private string _error= string.Empty;
         public string PageName => "Profile";
+
+
+        public string? Email
+        {
+            get => _email;
+
+            set
+            {
+                if (value == _email) return;
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public string Error => _error;
+
+        string IDataErrorInfo.this[string columnName]
+        {
+            get
+            {
+                if(columnName==nameof(Email))
+                {
+                    if (!EmailValidationRegex.IsValidEmail(Email))
+                    {
+                        return "Email is not valid!";
+                    }
+                }
+                return string.Empty;
+            }
+        }
     }
 }
